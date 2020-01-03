3faf9f68a331bdf2e603e320f6b484dad85df505
Leonardo Angelini <leonardo@cobaltica.it>
2020-01-03 23:23:30 +0100

    upd

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Build image"
-docker build --rm -f "Dockerfile" -t cobaltica/dotnet-sdk-angular:3.1.100 .
-echo "Tag image"
-docker tag cobaltica/dotnet-sdk-angular:3.1.100 cobaltica/dotnet-sdk-angular:latest
-echo "Publish"
-docker push cobaltica/dotnet-sdk-angular:3.1.100
-docker push cobaltica/dotnet-sdk-angular:latest
